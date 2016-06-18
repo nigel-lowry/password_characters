5be65564c65b6505198099c9802981ac7a04c2e6
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2016-06-18 08:34:07 +0100

    Simplify arguments

--- spec/password_and_ordinals_spec.rb
+++ spec/password_and_ordinals_spec.rb
@@ -2,14 +2,14 @@ require 'password_and_ordinals'
 
 RSpec.describe PasswordAndOrdinals do
   describe '#characters' do
-    subject { PasswordAndOrdinals.new('secret', [1, 2, 3]).characters }
+    subject { PasswordAndOrdinals.new('secret', [1, 2]).characters }
 
-    it { should eq(%w(s e c)) }
+    it { should eq(%w(s e)) }
   end
 
   it 'raises error when not in ascending order' do
     expect {
-      PasswordAndOrdinals.new('secret', [3, 2, 1])
-    }.to raise_error(ArgumentError).with_message('[3, 2, 1] is not in ascending order')
+      PasswordAndOrdinals.new('secret', [2, 1])
+    }.to raise_error(ArgumentError).with_message('[2, 1] is not in ascending order')
   end
 end
